4c495bf4be96389c4a64bc0066e9380b3a64f6ae
Jiyoung Jenna Yeo <ysy91231@gmail.com>
2021-04-26 20:40:28 +0900

    캐러셀 구현 중 실패상태

---
+++ enby_project/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    width: 1920px;
+    height: 83px;
+    background-color: grey;
+`;
+
+export default Header
---
+++ enby_project/src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+import Header from '../components/Header'
+
+
+function Main() {
+    return (
+        <div>
+            <Header />
+            <Carousel />
+        </div>
+    )
+}
+
+export default Main
